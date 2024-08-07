7e6981b6512b4e68f0dcbed631b054980d3c506d
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2024-08-07 14:01:30 -0600

    Ternary operator for Click singular word state

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -23,7 +23,7 @@ class _CounterScreenState extends State<CounterScreen> {
             Text('$clickCounter',
                 style: const TextStyle(
                     fontSize: 160, fontWeight: FontWeight.w100)),
-            const Text('Clicks', style: TextStyle(fontSize: 25))
+            Text('Click${ clickCounter == 1 ? '' : 's'}', style: const TextStyle(fontSize: 25))
           ],
         ),
       ),
